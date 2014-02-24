7366e66e65efcac2d4191ba3f55e11523e830c8a
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2014-02-24 11:29:43 +0000

    Porting using //eve/development/MAINLINE<-//eve/release/TRANQUILITY at head (726687)
    Binaries submitted in 727045
    Build time : 0 minutes 13.150000 seconds
    Branch : //eve/branches/development/MAINLINE/
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/evePathfinder/": change = 727021]

--- python/evePathfinder/test/test_pathfinder.py
+++ python/evePathfinder/test/test_pathfinder.py
@@ -150,7 +150,7 @@ class ClientPathfinderTestCase(unittest.TestCase):
         self.pathfinderCore.GetListOfWaypointPaths.return_value = path
         self.convertStationIDToSolarSystemIDIfNecessaryMethod.side_effect = lambda x: 11 if x == 15 else x
         path = self.clientPathfinder.GetWaypointPath(waypoints)
-        self.assertEqual([5, 3, 10, 10, 4, 11, 15], path)
+        self.assertEqual([5, 3, 10, 4, 11, 15], path)
 
     @mock.patch("evePathfinder.pathfinder.IsWormholeSystem")
     def testWaypointPathRespectsAutopilot(self, isWormholeSystem):
@@ -162,8 +162,52 @@ class ClientPathfinderTestCase(unittest.TestCase):
         self.pathfinderCore.GetListOfWaypointPaths.assert_called_once_with(
             self.stateInterface2, self.fromID, waypoints
         )
-        self.assertEqual([5, 3, 10, 10, 4, 11], path)
+        self.assertEqual([5, 3, 10, 4, 11], path)
 
+    def testGetCompleteWaypointList_ReturnsSolarSystemWaypointsIfNoNonSystemWaypointsAreSet(self):
+        systemIdWaypoints = [[1, 2, 3]]
+        waypoints = [1, 3]
+
+        self.assertEqual(systemIdWaypoints[0], self.clientPathfinder.GetCompleteWaypointList(systemIdWaypoints, waypoints))
+
+    def testGetCompleteWaypointList_ReturnsWaypointWithAddedStations(self):
+        origin = 1
+        destinationSystem = 2
+        destinationStation = 10
+
+        systemIdWaypoints = [[origin, destinationSystem]]
+        waypoints = [origin, destinationStation]
+
+        self.clientPathfinder.ConvertStationIDToSolarSystemIDIfNecessary = lambda x: 2 if x == 10 else x
+
+        self.assertEqual([origin, destinationSystem, destinationStation], self.clientPathfinder.GetCompleteWaypointList(systemIdWaypoints, waypoints))
+
+    def testGetCompleteWaypointList_ReturnsWaypointWithAllStationsInSystem(self):
+        originSolarSystem = 1
+        destinationSolarSystem = 2
+        destinationStation = 10
+        secondDestinationStation = 11
+
+        systemIdWaypoints = [[originSolarSystem, destinationSolarSystem], [destinationSolarSystem]]
+        waypoints = [originSolarSystem, destinationStation, secondDestinationStation]
+
+        self.clientPathfinder.ConvertStationIDToSolarSystemIDIfNecessary = lambda x: destinationSolarSystem if x in (destinationStation, secondDestinationStation) else x
+
+        self.assertEqual([originSolarSystem, destinationSolarSystem, destinationStation, secondDestinationStation], self.clientPathfinder.GetCompleteWaypointList(systemIdWaypoints, waypoints))
+
+    def testGetCompeleteWaypointList_DoesNotRepeatSameSolarsystemInRow(self):
+        o = 1
+        d1 = 3
+        d2 = 5
+        d3 = 7
+        systemIdWaypoints = [[o, d1], [d1, d2], [d2, d1, d3]]
+        waypoints = [o, d1, d2, d3]
+
+        self.assertEqual([o, d1, d2, d1, d3], self.clientPathfinder.GetCompleteWaypointList(systemIdWaypoints, waypoints))
+
+    def testGetCompleteWaypointList_ReturnsEmptyList_IfDestinationIsUnreachable(self):
+        unreachablePath = [[]]
+        self.assertEqual([], self.clientPathfinder.GetCompleteWaypointList(unreachablePath, [1, 2]))
 
     def testAddAvoidanceItemsSetsNewValue(self):
         avoided = [1, 2]
@@ -176,4 +220,3 @@ class ClientPathfinderTestCase(unittest.TestCase):
         self.stateInterface2.GetAvoidanceItems.return_value = avoided
         self.clientPathfinder.RemoveAvoidanceItem(2)
         self.stateInterface2.SetAvoidanceItems.assert_called_once_with([1, 3])
-
