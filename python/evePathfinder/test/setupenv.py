db7a63186b66b112f4e15a59dd73cd93a8ab361f
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2014-05-15 12:43:32 +0000

    fixing evePathfinder binbootstrapping
    Review: http://codecollaborator/ui#review:id=3286
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/evePathfinder/": change = 784222]

--- python/evePathfinder/test/setupenv.py
+++ python/evePathfinder/test/setupenv.py
@@ -1,8 +1,7 @@
 import site, os
-
 PACKAGE_ROOT = os.path.abspath(os.path.join(__file__, '../../..'))
 site.addsitedir(PACKAGE_ROOT)
 
 
 import binbootstrapper
-binbootstrapper.update_binaries(__file__, ('eve', 'pyEvePathfinder'))
+binbootstrapper.update_binaries(__file__, binbootstrapper.DLL_EVEPATHFINDER)
