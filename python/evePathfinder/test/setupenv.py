fcf22111802cdb5c6c77d579a44cd77209a5849d
CCP Aporia <28982391+CCP-Aporia@users.noreply.github.com>
2021-08-23 11:17:15 +0000

    Copy using //eve/development/MAINLINE->//eve/sandbox/NATIVE-MAC-CLIENT
    This brings the NATIVE-MAC-CLIENT branch into MAINLINE. Changes are extensive, a summary exists on the wiki at: https://wiki.ccpgames.com/display/TE/NMC+branch+major+change+overview
    
    https://jira.ccpgames.com/browse/EO-19054
    
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/evePathfinder/": change = 1929513]

--- python/evePathfinder/test/setupenv.py
+++ python/evePathfinder/test/setupenv.py
@@ -1,7 +1,3 @@
 import site, os
 PACKAGE_ROOT = os.path.abspath(os.path.join(__file__, '../../..'))
 site.addsitedir(PACKAGE_ROOT)
-
-
-import binbootstrapper
-binbootstrapper.update_binaries(__file__, binbootstrapper.DLL_EVEPATHFINDER)
