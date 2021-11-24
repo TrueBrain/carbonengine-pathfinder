389acf009f012bc84f5c46c0c1d49a54e28e8b56
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2021-11-24 18:55:44 +0000

    AIR NPE: Investigate potential optimization for CreatePathfinder on space scene initialization https://jira.ccpgames.com/browse/EO-24004
    [git-p4: depot-paths = "//eve/branches/development/MAINLINE/packages/evePathfinder/": change = 1971280]

--- python/evePathfinder/factory.py
+++ python/evePathfinder/factory.py
@@ -3,13 +3,14 @@ Contains factory methods to construct a pathfinder core instance
 """
 
 import pyEvePathfinder
+from carbon.common.lib import telemetry
 
-from security.common.util import get_modified_security_level
+from threadutils.be_nice import be_nice
 from . import core
-from inventorycommon.util import IsWormholeRegion
 
 
-def CreatePathfinder(mapRegionCache, mapSystemCache, mapJumpCache, extraJumps=()):
+@telemetry.ZONE_METHOD
+def CreatePathfinder(mapRegionCache, mapSystemCache, mapJumpCache, get_security_level_func, extraJumps=()):
     """
     returns pathinder initialized with an eve map and jump data
     """
@@ -19,18 +20,22 @@ def CreatePathfinder(mapRegionCache, mapSystemCache, mapJumpCache, extraJumps=()
 
         for constellationID in regionItem.constellationIDs:
             eveMap.CreateConstellation(constellationID, regionID)
+            be_nice()
 
     for solarSystemID, ssInfo in mapSystemCache.iteritems():
-        securityLevel = get_modified_security_level(solarSystemID)
+        securityLevel = get_security_level_func(solarSystemID)
         eveMap.CreateSolarSystem(solarSystemID, ssInfo.constellationID, securityLevel)
+        be_nice()
 
     # Once populated, create the jumps
     for jump in mapJumpCache:
         eveMap.AddJump(jump.fromSystemID, jump.toSystemID, jump.stargateID)
         eveMap.AddJump(jump.toSystemID, jump.fromSystemID, jump.stargateID)  # <-- TODO: stargateID wrong!)
+        be_nice()
 
     for fromSystemID, toSystemID, stargateID in extraJumps:
         eveMap.AddJump(fromSystemID, toSystemID, toSystemID) # <-- TODO: stargateID wrong! the function can't take bigint, doesn't seem to matter)
+        be_nice()
 
     eveMap.Finalize()
 
