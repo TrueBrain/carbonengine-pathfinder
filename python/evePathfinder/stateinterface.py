92a0f85d3f39b407d5d620ae3f3c64f4c80158c0
Anonymous CCP Employee <anonymous.employee@ccpgames.com>
2024-02-20 14:14:13 +0000

    Fix encoding for hashing pathfinder state https://ccpgames.atlassian.net/browse/PLAT-4953

--- python/evePathfinder/stateinterface.py
+++ python/evePathfinder/stateinterface.py
@@ -13,16 +13,15 @@ from evePathfinder.pathfinderconst import DEFAULT_SECURITY_PENALTY
 
 # Systems that the autopilot will avoid by default
 DEFAULT_AVOIDANCE = [
-    30000142,  # Jita
 ]
 
 
 def GetCurrentStateHash(stateInterface, fromSolarSystemID):
     m = hashlib.md5()
-    m.update(str(fromSolarSystemID))
-    m.update(stateInterface.GetRouteType())
-    m.update(str(stateInterface.GetSecurityPenalty()))
-    m.update(str(stateInterface.GetAvoidanceList()))
+    m.update(str(fromSolarSystemID).encode('utf-8'))
+    m.update(stateInterface.GetRouteType().encode('utf-8'))
+    m.update(str(stateInterface.GetSecurityPenalty()).encode('utf-8'))
+    m.update(str(stateInterface.GetAvoidanceList()).encode('utf-8'))
     return m.hexdigest()
 
 
