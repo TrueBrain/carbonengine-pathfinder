cf8131ef875170a65d51e3b4d836feb8e4193c14
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2026-01-13 14:31:17 +0000

    Change Destiny to Pathfinder in kotlin files

--- .teamcity/_Self.kt/Project.kt
+++ .teamcity/_Self.kt/Project.kt
@@ -8,7 +8,7 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object Project : Project({
 
-    description = "Build / Publish pipeline for https://github.com/ccpgames/carbon-destiny"
+    description = "Build / Publish pipeline for https://github.com/ccpgames/carbon-pathfinder"
 
     params {
         /* before changing carbon_ref, make sure to disable automatic settings synchronization on teamcity */
