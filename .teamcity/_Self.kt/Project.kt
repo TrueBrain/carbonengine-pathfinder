f153aeb31b8a83e98c72d77110fef69ab93fc16a
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2026-01-13 14:10:06 +0000

    Add latest changes from carbon_template

--- .teamcity/_Self.kt/Project.kt
+++ .teamcity/_Self.kt/Project.kt
@@ -8,9 +8,10 @@ import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
 
 object Project : Project({
 
-    description = "Build / Publish pipeline for https://github.com/ccpgames/carbon-pathfinder"
+    description = "Build / Publish pipeline for https://github.com/ccpgames/carbon-destiny"
 
     params {
+        /* before changing carbon_ref, make sure to disable automatic settings synchronization on teamcity */
         param("carbon_ref", "refs/heads/main")
         param("carbon-pipeline-tools-ref", "refs/heads/main")
     }
