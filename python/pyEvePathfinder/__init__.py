7e0c32e5da1c644ea86aa0047d17b01c81f8be35
CCP Toebeans <105929497+ccptoebeans@users.noreply.github.com>
2026-03-09 14:19:43 +0000

    Enable tests by default

--- python/pyEvePathfinder/__init__.py
+++ python/pyEvePathfinder/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) CCP 2012
 
 """
-Wrapper module to access blue exposed things from EVE's evePathfinder DLL.
+Wrapper module to access blue exposed things from EVE's pyEvePathfinder DLL.
 """
 
 import blue
